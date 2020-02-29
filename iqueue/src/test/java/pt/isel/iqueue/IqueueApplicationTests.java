4dd83ed0d625021ac65b47636fd0653b91795f6e
ISEL-A40569 <40569@alunos.isel.ipl.pt>
2020-02-29 17:29:00 +0000

    Spring project setup

---
+++ iqueue/src/test/java/pt/isel/iqueue/IqueueApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.isel.iqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IqueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
