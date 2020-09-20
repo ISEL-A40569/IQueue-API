cf36c0e83e597c9c73ea2ca0037c0aa55da3d104
ISEL-A40569 <40569@alunos.isel.ipl.pt>
2020-09-20 20:03:43 +0100

    GetOperatorUsers filtered by userProfileId.

--- iqueue/src/main/java/pt/ipl/isel/ps/iqueue/controller/OperatorUserController.java
+++ iqueue/src/main/java/pt/ipl/isel/ps/iqueue/controller/OperatorUserController.java
@@ -4,13 +4,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pt.ipl.isel.ps.iqueue.dao.OperatorUserDao;
+import pt.ipl.isel.ps.iqueue.dao.UserDao;
 import pt.ipl.isel.ps.iqueue.dao.embeddable.OperatorUserIds;
 import pt.ipl.isel.ps.iqueue.mapping.OperatorUserDaoModelMapper;
 import pt.ipl.isel.ps.iqueue.model.OperatorUser;
 import pt.ipl.isel.ps.iqueue.repository.OperatorUserRepository;
+import pt.ipl.isel.ps.iqueue.repository.UserRepository;
 import pt.ipl.isel.ps.iqueue.utils.ErrorNotificationService;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/api/iqueue/operator")
@@ -25,21 +30,34 @@ public class OperatorUserController extends Controller<OperatorUser, OperatorUse
     @Autowired
     private final ErrorNotificationService errorNotificationService;
 
-    public OperatorUserController(OperatorUserRepository operatorUserRepository, OperatorUserDaoModelMapper operatorUserDaoModelMapper, ErrorNotificationService errorNotificationService) {
+    @Autowired
+    private final UserRepository userRepository;
+
+    public OperatorUserController(OperatorUserRepository operatorUserRepository, OperatorUserDaoModelMapper operatorUserDaoModelMapper, ErrorNotificationService errorNotificationService, UserRepository userRepository) {
         super(operatorUserRepository, operatorUserDaoModelMapper, errorNotificationService);
         this.operatorUserRepository = operatorUserRepository;
         this.operatorUserDaoModelMapper = operatorUserDaoModelMapper;
         this.errorNotificationService = errorNotificationService;
+        this.userRepository = userRepository;
     }
 
     @GetMapping(value = "{operatorId}/user", headers = {"Accept=application/json"})
-    public ResponseEntity getOperatorUsers(@PathVariable int operatorId) {
-        return super.getSome(operatorUserRepository
+    public ResponseEntity getOperatorUsers(@PathVariable int operatorId, @RequestParam(required = false) Integer userProfileId) {
+        List<OperatorUserDao> operatorUserList = operatorUserRepository
                 .findAll()
                 .stream()
                 .filter(operatorUserDao -> operatorUserDao.getOperatorUserIds().getOperatorId() == operatorId)
-                .collect(Collectors.toList())
-        );
+                .collect(Collectors.toList());
+
+        if (userProfileId != null) {
+            operatorUserList = operatorUserList.stream()
+                    .filter(operatorUserDao -> {
+                        Optional<UserDao> optionalUserDao = userRepository.findById(operatorUserDao.getOperatorUserIds().getUserId());
+                        return optionalUserDao.get().getUserProfileId() == userProfileId;
+                    }).collect(Collectors.toList());
+        }
+
+        return super.getSome(operatorUserList);
     }
 
     @GetMapping(value = "user/{userId}", headers = {"Accept=application/json"})
